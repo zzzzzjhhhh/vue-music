20431042c8c9ec314054b9095d90fd7097b10fc0
zzj <zhengzijian56@gmail.com>
2020-07-22 15:55:51 +0100

    获取singer-detail页面数据

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -6,12 +6,28 @@
 
 <script>
 import { mapGetters } from "vuex";
+import { getSingerDetail } from "api/singer";
+import { ERR_OK } from "api/config";
+
 export default {
   computed: {
     ...mapGetters({ singer: "getSinger" })
   },
   created() {
-    console.log(this.singer);
+    this._getSingerDetail();
+  },
+  methods: {
+    _getSingerDetail() {
+      if (!this.singer.id) {
+        this.$router.push("/singer");
+        return;
+      }
+      getSingerDetail(this.singer.id).then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list);
+        }
+      });
+    }
   }
 };
 </script>
